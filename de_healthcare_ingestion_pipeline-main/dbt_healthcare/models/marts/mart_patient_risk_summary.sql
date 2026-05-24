501f4ff55189b0ebf364d2bc4e31fa92d9107ba0
Virda Akhtar <virdakhtr30@gmail.com>
2026-05-25 00:51:01 +0500

    Updated patient risk mart for dashboard KPIs

--- de_healthcare_ingestion_pipeline-main/dbt_healthcare/models/marts/mart_patient_risk_summary.sql
+++ de_healthcare_ingestion_pipeline-main/dbt_healthcare/models/marts/mart_patient_risk_summary.sql
@@ -1,7 +1,83 @@
+-- select
+--     event_date,
+--     count(*) as total_patient_records,
+--     avg(risk_score) as avg_patient_risk_score,
+--     sum(is_high_risk) as high_risk_patient_count
+-- from {{ ref('int_patient_risk_enriched') }}
+-- group by event_date
+
+-- =============================================================
+
+-- select
+--     event_date,
+
+--     count(distinct patient_id) as monitored_patients,
+
+--     avg(risk_score) as avg_risk_score,
+
+--     sum(
+--         case
+--             when risk_score >= 14
+--             then 1
+--             else 0
+--         end
+--     ) as high_risk_monitoring_load,
+
+--     sum(encounter_count) as daily_patient_encounters
+
+-- from {{ ref('int_patient_risk_enriched') }}
+
+-- group by event_date
+
+-- =============================================================
+
+-- with ranked_patients as (
+--     select
+--         *,
+--         percent_rank() over (order by risk_score) as risk_percentile
+--     from {{ ref('int_patient_risk_enriched') }}
+-- )
+
+-- select
+--     event_date,
+
+--     count(distinct patient_id) as monitored_patients,
+
+--     avg(risk_score) as avg_risk_score,
+
+--     sum(
+--         case
+--             when risk_percentile >= 0.90
+--             then 1
+--             else 0
+--         end
+--     ) as high_risk_monitoring_load
+
+-- from ranked_patients
+
+-- group by event_date
+
+-- =============================================================
+
 select
     event_date,
-    count(*) as total_patient_records,
-    avg(risk_score) as avg_patient_risk_score,
-    sum(is_high_risk) as high_risk_patient_count
+
+    count(distinct patient_id) as monitored_patients,
+
+    avg(risk_score) as avg_risk_score,
+
+    sum(
+        case
+            when risk_score >= 15
+            then 1
+            else 0
+        end
+    ) as high_risk_monitoring_load,
+
+    sum(diagnosis_risk_flag) as high_risk_diagnoses,
+
+    sum(mortality_history_flag) as mortality_linked_cases
+
 from {{ ref('int_patient_risk_enriched') }}
+
 group by event_date
